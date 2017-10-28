cf3b7ee7348b48221f047e025008da4b3009c948
lolxy <lolxy@users.noreply.github.com>
2017-10-28 17:47:15 +0800

    新增头部内容
    新增头部内容

---
+++ src/layouts/default.js
@@ -0,0 +1,9 @@
+import pageHeader from '@/components/header/main.vue'
+import vNavbar from '@/components/navbar/main.vue'
+
+export default {
+  components: {
+    pageHeader,
+    vNavbar
+  }
+}
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,18 +1,3 @@
-<template>
-  <div class="wrapper">
-    <page-header></page-header>
-    <div class="page-main">
-      <slot></slot>
-    </div>
-  </div>
-</template>
-
-<script>
-import pageHeader from '@/components/header/main.vue'
-
-export default {
-  components: {
-    pageHeader
-  }
-}
-</script>
+<template src="./default.html"></template>
+<script src="./default.js" lang="babel"></script>
+<style src="./default.scss" lang="scss"></style>
