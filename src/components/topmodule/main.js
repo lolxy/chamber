027eb8e126b7600e9a0a2918f98c235e21dcc212
lolxy <lolxy@users.noreply.github.com>
2017-11-01 11:40:45 +0800

    商会首页数据调用
    商会首页数据调用

--- src/components/topmodule/main.js
+++ src/components/topmodule/main.js
@@ -3,23 +3,16 @@
  * ============
  */
 
- import Vue from 'vue';
- import store from '@/store';
- import authService from '@/services/auth';
+import Vue from 'vue';
+import store from '@/store';
+import authService from '@/services/auth';
 import slide from '@/components/slide/main.vue';
 import news from '@/components/news/main.vue';
-import VueAwesomeSwiper from 'vue-awesome-swiper';
-Vue.use(VueAwesomeSwiper)
 
 export default {
   data() {
     return {
-      swiperOption: {
-        loop: true,
-        autoplay: 6000,
-        pagination : '.swiper-pagination',
-        paginationClickable :true
-      }
+
     }
   },
   components: {
